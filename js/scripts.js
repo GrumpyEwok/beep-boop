91db3ebdf76fae542a416a65b648ef15553b5bcc
GrumpyEwok <evanfilkins@gmail.com>
2018-08-17 09:26:07 -0700

    Properly log input.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//Front-End
+
+$(document).ready(function(){
+  $("#input-form").submit(function(event){
+    event.preventDefault();
+
+  var numToCount = $("#userInput").val();
+    console.log(numToCount)
+
+  });
+
+});
+
+//Variable Definitions
+
+// Back-End
