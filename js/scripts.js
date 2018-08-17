6acfcab6fc3f26e1e639af3fde5af004298b08b4
GrumpyEwok <evanfilkins@gmail.com>
2018-08-17 09:56:27 -0700

    JS stores and displays user input.

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,17 @@ $(document).ready(function(){
   $("#input-form").submit(function(event){
     event.preventDefault();
 
-  var numToCount = $("#userInput").val();
+var numToCount = $("#userInput").val();
+var result = numToCount
     console.log(numToCount)
+    console.log(result)
+    $("#display-field").show();
+    $("#display-field").text(result);
 
   });
-
 });
+
+
 
 //Variable Definitions
 
