9dd64424c8180529bbb5d60a9312743bc0134009
GrumpyEwok <evanfilkins@gmail.com>
2018-08-17 11:46:45 -0700

    Return phrase when user inputs a value divisible by 3.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   $("#input-form").submit(function(event){
     event.preventDefault();
 var initialInput = $("#userInput").val();
+var initialInputInteger = parseInt($("#userInput").val())
     if (initialInput === "0") {
       $("#display-field").show();
       $("#display-field").text("Beep!");
@@ -10,6 +11,10 @@ var initialInput = $("#userInput").val();
     else if (initialInput === "1") {
       $("#display-field").show();
       $("#display-field").text("Boop!");
+    }
+      else if (initialInputInteger % 3 === 0) {
+      $("#display-field").show();
+      $("#display-field").text("I'm sorry, Dave. I'm afraid I can't do that.");
     }
     else {
       $("#display-field").show();
